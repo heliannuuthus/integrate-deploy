9dba105039694c539a985e49761372f0c4c94763
heliannuuthus <heliannuuthus@gmail.com>
2024-03-17 11:11:57 +0800

    springcloud service

---
+++ tests/gradle-cloud/src/main/java/io/ghrc/heliannuuthus/gradlecloud/GradleCloudApplication.java
@@ -0,0 +1,14 @@
+package io.ghrc.heliannuuthus.gradlecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleCloudApplication {
+    public static void main(String[] args) {
+        // https://docs.spring.io/spring-boot/docs/2.1.5.RELEASE/reference/html/using-boot-devtools.html#using-boot-devtools-restart-disable
+        System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(GradleCloudApplication.class, args);
+    }
+
+}
---
+++ tests/gradle-cloud/src/test/java/io/ghrc/heliannuuthus/gradlecloud/GradleCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ghrc.heliannuuthus.gradlecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
